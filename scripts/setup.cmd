4d23e9d16c9060a969fcbab42e5f4f776b396c65
karvashish <kartikeyavashishtha@gmail.com>
2024-10-27 22:38:01 +0530

    remove openssl

--- scripts/setup.cmd
+++ scripts/setup.cmd
@@ -1,13 +1,5 @@
 @echo off
 
-REM Step 1: Build and Run OpenSSL Dockerfile
-cd ./containers/openssl
-
-if not exist ../../certificates mkdir ../../certificates
-
-podman build -t minimal_openssl_image .
-podman run --rm -v "%cd%\..\..\certificates:/certificates" minimal_openssl_image
-
-REM Step 2: Start all services with Podman Compose
-cd ..
+REM Start all services with Podman Compose
+cd ./containers
 podman-compose up -d
--- scripts/teardown.cmd
+++ scripts/teardown.cmd
@@ -3,15 +3,7 @@
 cd containers
 podman-compose down
 
-REM Remove the OpenSSL image
-podman rmi minimal_openssl_image
-
 REM Remove volumes created by Podman Compose
 podman volume prune -f
-
-REM Clean up certificates folder
-cd ../certificates
-rmdir /s /q root
-rmdir /s /q intermediate
 
 echo Teardown completed successfully.
