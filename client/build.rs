20caaac778739f7b91b6b441c0b431fffd25fc17
karvashish <kartikeyavashishtha@gmail.com>
2024-12-31 09:12:10 +0530

    some docs

--- client/build.rs
+++ client/build.rs
@@ -2,6 +2,21 @@ use std::path::Path;
 use std::process::Command;
 use std::{env, process};
 
+// This build script performs several tasks during the build process:
+// 1. It monitors specific environment variables (PUBKEY, IFCNAME, ADDR, CIDR) and ensures
+//    that Cargo re-runs this script if any of them change.
+// 2. It retrieves and validates these environment variables:
+//    - PUBKEY: Optional, used for embedding a public key into the binary.
+//    - IFCNAME: Optional, defaults to "wg0" if not set.
+//    - ADDR and CIDR: Mandatory, representing an IPv4 address and CIDR subnet mask.
+//      The script terminates the build with an error if either is missing or invalid.
+// 3. After validation, the values of these variables are embedded into the binary at compile time
+//    using `cargo:rustc-env`, ensuring the application doesn't depend on runtime environment variables.
+// 4. The script compiles a C source file (`wireguard.c`) into a static library using GCC and AR tools:
+//    - The C file is compiled into an object file (`wireguard.o`).
+//    - The object file is archived into a static library (`libwireguard.a`).
+// 5. Cargo is instructed to link the Rust project with the static library and re-run this script if
+//    the `wireguard.c` file changes.
 fn main() {
     println!("cargo:rerun-if-env-changed=PUBKEY");
     println!("cargo:rerun-if-env-changed=IFCNAME");
